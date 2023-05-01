0be99837853061b294c3b6ff5e1cac4890958aef
Gaston Sucharzewski <jugasu06@gmail.com>
2023-05-01 19:11:58 -0300

    v0

---
+++ Entregables/3/src/ProductManager.js
@@ -0,0 +1,90 @@
+const fs = require("fs").promises;
+
+class ProductManager {
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.products = this.getProducts();
+    const lastProductId = this.products.length > 0 ? this.products[products.length - 1].id : 0;
+    ProductManager.productGlobalID = lastProductId;
+  }
+  static productGlobalID = 0;
+
+  async getProducts() {
+    try {
+      const data = await fs.readFile(this.filePath, "utf-8");
+      return JSON.parse(data);
+    } catch (err) {
+      return [];
+    }
+  };
+
+  async addProduct(title, description, price, thumbnail, stock, code) {
+    const products = await this.getProducts();
+    const controlCode = products.some((product) => product.code === code);
+    if (controlCode) {
+      console.log("El producto que desea ingresar ya existe");
+      return null;
+    }
+
+    let newProduct = {
+      id: ProductManager.productGlobalID + 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    while (products.some((product) => product.id === newProduct.id)) {
+      newProduct.id = ProductManager.productGlobalID + 1;
+      ProductManager.productGlobalID++;
+    }
+
+    ProductManager.productGlobalID++;
+    products.push(newProduct);
+    await fs.writeFile(this.filePath, JSON.stringify(products));
+    console.log(`El producto de id ${newProduct.id}, nombre ${newProduct.title}. fue ingresado correctamente.`);
+    return newProduct;
+  };
+
+  async getProductById(id) {
+    const products = await this.getProducts();
+    const product = products.find((product) => product.id == id);
+    if (product) {
+      return product;
+    } else {
+      throw new Error(`Product with id ${id} not found`);
+    }
+  }; 
+
+  async updateProduct(id, fieldsToUpdate) {
+    const products = await this.getProducts();
+    const index = products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const prodModified = {
+      ...products[index],
+      ...fieldsToUpdate,
+      id: products[index].id,
+    };
+    products.splice(index, 1, prodModified);
+    await fs.writeFile(this.filePath, JSON.stringify(products));
+    return prodModified;
+  };
+
+  async deleteProduct(id) {
+    const products = await this.getProducts();
+    const index = products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      return console.log(`No existe producto para el id ${id}.`);
+    }
+    const deletedProduct = products.splice(index, 1)[0];
+    await fs.writeFile(this.filePath, JSON.stringify(products, 2));
+    console.log(`El Producto de ID ${id} fue borrado.`);
+    return deletedProduct;
+  };
+}
+//const products = new ProductManager("./products.json");
+module.exports = ProductManager;
---
+++ Entregables/3/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const ProductManager = require('./ProductManager');
+const app = express();
+const port = 8080;
+const productManager = new ProductManager("./products.json");
+
+//TRAIGO TODOS LOS PROD (en caso de tener limite, trae solo la cantidad indicada)
+app.get('/products', async (req, res) => {
+  const limit = req.query.limit;
+  const products = await productManager.getProducts();
+  
+  if (limit) {
+    res.status(200).json(products.slice(0, limit));
+  } else {
+    res.status(404).json(products);
+  }
+});
+
+//TRAIGO PRODUCTO SEGUN EL ID INDICADO EN URL
+app.get('/products/:pid', async (req, res) => {
+  const pid = req.params.pid;
+  try {
+    const product = await productManager.getProductById(pid);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
