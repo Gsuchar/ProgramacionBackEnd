5fc236b9f2bfdca59bf994742fe10a40b293eb18
Gsuchar <jugasu06@gmail.com>
2023-05-28 19:54:14 -0300

    .

---
+++ Entregables/6_Practica-Integradora_1/src/dao/models/messageModel.js
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+   
+    // code: { type: Number/*, required: true, max: 100, unique: true */},
+    // category: { type: String/*,required: true, max: 100 */},
+    ////////////////////////////////////////////////////////////////////////
+    //{user:correoDelUsuario, message: mensaje del usuario}
+    user: String,
+    message: String
+});
+
+export const MessageModel = model("messages", schema);
