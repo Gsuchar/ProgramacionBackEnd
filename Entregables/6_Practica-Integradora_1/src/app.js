79d9a4e2c43f52ee1fae42a41e0591ebbd067190
Gsuchar <jugasu06@gmail.com>
2023-05-31 22:26:01 -0300

    .

--- Entregables/6_Practica-Integradora_1/src/app.js
+++ Entregables/6_Practica-Integradora_1/src/app.js
@@ -5,103 +5,46 @@ import chatRoutes from './routes/chatRoutes.js';
 import { __dirname } from "./utils.js";
 import path from "path";
 import handlebars from "express-handlebars";
-import { Server } from "socket.io";
 import http from 'http';
-import { ProductManager } from "./dao/ProductManager.js";
-import { connectMongo } from "./utils.js";
-import { MessageModel } from './dao/models/messageModel.js';
+import { connectMongo, socketServerHandler } from "./utils.js";
 
-const productManager = new ProductManager('./src/dao/dataFiles/products.json');
+/***************************************************************************/
+
 
 const app = express();
 const port = 8080;
-//mongo db
+
+// Connect to MongoDB
 connectMongo();
 
+// Create HTTP server
+const httpServer = http.createServer(app);
 
-//MIDDLEWARES
+// Listen to HTTP server
+httpServer.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
+
+// Socket server handler
+socketServerHandler(httpServer);
+
+// MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-const httpServer = http.createServer(app);
-const socketServer = new Server(httpServer);
-//global mensajes
-let mens=[]; 
 
-//ESCUCHA HTTPSERVER
-httpServer.listen(port, () => {
-  console.log(`Server corriendo en puerto: ${port}`);
-});
-
-
-//ENGINE HANDLEBARS
+// ENGINE HANDLEBARS
 app.engine("handlebars", handlebars.engine());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "handlebars");
 
-//Routes
+// Routes
 app.use('/', productRoutes);
 app.use('/', cartRoutes);
 app.use('/', chatRoutes);
 
-//HANDLERS SOCKET
 
-socketServer.on("connection", (socket) => {
-  console.log("Un cliente se ha conectado: " + socket.id);
-  /******** PRODUCTS **********/
-  socket.on("new-product", async (newProd) => {
-    try {
-      await productManager.addProduct({ ...newProd });
-      const productsList = await productManager.getProducts();
-      socketServer.emit("products", productsList );
-      //console.log(productsList);
-      //console.log(`Se ingreso el producto ${newProd.title} correctamente` );
-    } catch (err) {
-      console.log({ Error: `${err}` });
-      
-    }
-  });
-
-  socket.on("delete-product", async (productId) => {
-    try {
-        await productManager.deleteProduct(productId);
-        const productsUpdt = await productManager.getProducts();
-        socketServer.emit("products", productsUpdt);
-        //console.log(`Producto id ${productId} borrado correctamente` );        
-    } catch (err) {
-      console.log({ Error: `${err}` });      
-    }
-  });
-  /****** FIN PRODUCTS *****/
-
-  /** CHAT */ 
-  socket.on("message", async (msg) => {
-    try {
-      //unshift es parecido a push
-      //mens.unshift(men);
-      // Guardar el mensaje en la base de datos
-    await MessageModel.create(msg);
-
-    // Emitir los mensajes actualizados a todos los clientes
-    const msgs = await MessageModel.find();
-    socketServer.emit('messageLogs', msgs);
-  } catch (err) {
-    console.log({ Error: `${err}` });
-  }
-});
-  /** FIN CHAT */
-});
-//Variable global para mensajes del chat
-
-
-
-
-//PARA MOSTRAR ERROR CUANDO NO ENCUENTRA URL
+// Handle 404 Not Found
 app.get("*", (req, res) => {
-  return res.status(404).json({
-    status: "error",
-    msg: "no encontrado",
-    data: {},
-  });
+  return res.status(404).json({status: "error", msg: "Not found", data: {} });
 });
-
--- Entregables/6_Practica-Integradora_1/src/utils.js
+++ Entregables/6_Practica-Integradora_1/src/utils.js
@@ -1,6 +1,7 @@
+/***************************************************************************/
+//----------------MULTER----------------------------------------------------
 import multer from "multer";
-import { connect } from "mongoose";
-//app.use(express.static(path.join(__dirname, "public")));
+//-----
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "public"));
@@ -13,21 +14,86 @@ const storage = multer.diskStorage({
 export const uploader = multer({ storage });
 
 
-// https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+/***************************************************************************/
+//----------------__DIRNAME-------------------------------------------------
 import path from "path";
 import { fileURLToPath } from "url";
+//-----
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
-//DB
+
+/***************************************************************************/
+//----------------MONGO-----------------------------------------------------
+import { connect } from "mongoose";
+//-----
 export async function connectMongo() {
   try {
-    await connect(//en este caso ecommerce es el nombre de la BASE 
+    await connect(
       "mongodb+srv://Gsuchar:1J0pqk2HPyyEZZl4@progbackend.muru6sp.mongodb.net/ecommerce?retryWrites=true&w=majority"
     );
-    console.log("plug to mongo!");
+    console.log("Connected to MongoDB!");
   } catch (e) {
     console.log(e);
-    throw "can not connect to the db";
+    throw "Unable to connect to the database";
   }
-};
+}
+
+
+/***************************************************************************/
+//----------------SOCKET SERVER HANDLER-------------------------------------
+import { Server } from "socket.io";
+import { ProductManager } from "./dao/ProductManager.js";
+import { MessageModel } from './dao/models/messageModel.js';
+//-----
+export function socketServerHandler(httpServer) {
+  const socketServer = new Server(httpServer);
+  const productManager = new ProductManager('./src/dao/dataFiles/products.json');
+
+  // Listen for connections
+  socketServer.on("connection", (socket) => {
+    console.log("A client has connected: " + socket.id);
+
+    /******** PRODUCTS **********/
+    socket.on("new-product", async (newProd) => {
+      try {
+        await productManager.addProduct({ ...newProd });
+        const productsList = await productManager.getProducts();
+        socketServer.emit("products", productsList);
+        //console.log(productsList);
+        //console.log(`Product ${newProd.title} added successfully`);
+      } catch (err) {
+        console.log({ Error: `${err}` });
+      }
+    });
+
+    socket.on("delete-product", async (productId) => {
+      try {
+        await productManager.deleteProduct(productId);
+        const productsUpdt = await productManager.getProducts();
+        socketServer.emit("products", productsUpdt);
+        //console.log(`Product with ID ${productId} deleted successfully`);
+      } catch (err) {
+        console.log({ Error: `${err}` });
+      }
+    });
+    /****** END PRODUCTS *****/
+
+    /** CHAT */
+    socket.on("message", async (msg) => {
+      try {
+        //unshift is similar to push
+        //mens.unshift(men);
+        // Save the message in the database
+        await MessageModel.create(msg);
+
+        // Emit updated messages to all clients
+        const msgs = await MessageModel.find();
+        socketServer.emit('messageLogs', msgs);
+      } catch (err) {
+        console.log({ Error: `${err}` });
+      }
+    });
+    /** END CHAT */
+  });
+}
