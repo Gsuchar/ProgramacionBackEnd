8797b36d2d33462690b3ae973144f598b228f0d4
Gsuchar <jugasu06@gmail.com>
2023-06-07 19:44:43 -0300

    2daEntrega-PF

---
+++ Entregables/7_2daEntrega-ProyectoFinal/src/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
+import chatRoutes from './routes/chatRoutes.js';
+import { __dirname } from "./utils.js";
+import path from "path";
+import handlebars from "express-handlebars";
+import http from 'http';
+import { connectMongo, socketServerHandler } from "./utils.js";
+//-----
+
+
+const app = express();
+const port = 8080;
+
+// Connect a MongoDB/Atlas
+connectMongo();
+
+// Crea HTTP server
+const httpServer = http.createServer(app);
+
+// Anuncia puerto
+httpServer.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
+
+// Socket server handler
+socketServerHandler(httpServer);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Engine Handlebars para views
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+
+// Routes
+app.use('/', productRoutes);
+app.use('/', cartRoutes);
+app.use('/', chatRoutes);
+
+// Epic link Fail - 404 Not Found
+app.get("*", (req, res) => {
+  return res.status(404).json({status: "Epic error", msg: "Not found :( ", data: {} });
+});
---
+++ Entregables/7_2daEntrega-ProyectoFinal/src/dao/models/productModel.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({        
+    // title: { type: String/*, required: true, max: 100*/ },
+    // description: { type: String/*, required: true, max: 100 */},
+    // code: { type: Number/*, required: true, max: 100, unique: true */},
+    // price: { type: Number/*, required: true, max: 100*/ },
+    // status: { type: Boolean},
+    // stock: { type: Number/*, required: true, max: 100*/ },   
+    // category: { type: String/*,required: true, max: 100 */},
+    // thumbnail: { type: String}
+
+    title : String,
+    description: String ,
+    code:  Number,
+    price: Number,
+    status:  Boolean,
+    stock: Number,   
+    category: String,
+    thumbnail:  String
+},{versionKey:false});//{versionKey:false} saca __v: que es para versiones por moongose
+
+export const ProductModel = model("products", schema);
