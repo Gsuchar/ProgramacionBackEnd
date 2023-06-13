0027aff259f89e8fbb370589d1a45f78074308cc
Gsuchar <jugasu06@gmail.com>
2023-06-12 21:59:54 -0300

    SegundaEntregaPF

--- Entregables/6_Practica-Integradora_1/src/dao/models/cartModel.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const schema = new Schema({
-      
-   products: [ { idProduct: { type: String }, quantity: { type: Number } } ]
-
-},{versionKey:false});//{versionKey:false} saca __v: que es para versiones por moongose
-
-export const CartModel = model("carts", schema);
