d179f2c150afcadffcc6f098c8bb9a89441ed768
Gsuchar <jugasu06@gmail.com>
2023-06-17 19:02:21 -0300

    ENTREGA_8

---
+++ Entregables/8_Login_From__Passport/src/dao/models/productModel.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+//--
+
+const schema = new Schema({        
+    title: { type: String/*, required: true, max: 100*/ },
+    description: { type: String/*, required: true, max: 100 */},
+    code: { type: Number/*, required: true, max: 100, unique: true */},
+    price: { type: Number/*, required: true, max: 100*/ },
+    status: { type: Boolean},
+    stock: { type: Number/*, required: true, max: 100*/ },   
+    category: { type: String , index: true/*,required: true, max: 100 */},
+    thumbnail: { type: String}  
+},{versionKey:false});//{versionKey:false} saca __v: que es para versiones por moongose
+
+schema.plugin(mongoosePaginate);
+export const ProductModel = model("products", schema);
---
+++ Entregables/8_Login_From__Passport/src/public/js/indexProductsToCart.js
@@ -0,0 +1,67 @@
+const socket = io();
+
+
+socket.on("updatedProducts", (listProducts) => {
+  const tableBody = document.getElementById("dinamic-product-list");
+  const tableRows = listProducts.docs.map((product) => `
+    <tr>
+      <th scope="row">${product.id}</th>      
+      <td>${product.title}</td>
+      <td>${product.description}</td>
+      <td>${product.price}</td>
+      <td>${product.code}</td>
+      <td>${product.stock}</td>
+      <td>${product.category}</td>
+      <td>${product.thumbnail}</td>
+      <td><input type="submit" value="Agregar" class="btn btn-success" onclick="addToCart('${product.id}')"/></td>
+    </tr>
+  `);
+
+  tableBody.innerHTML = tableRows.join("");
+  const pagination = document.querySelector('.pagination');
+  pagination.innerHTML = '';
+
+  // Si existe prevPage del populate se muestra
+  const previousPage = listProducts.hasPrevPage
+    ? `<li class="page-item"><a class="page-link" href="#" onclick="onFilterChange(${listProducts.prevPage})"><-- </a></li>`
+    : '';
+  // Si existe nextPage del populate se muestra
+  const nextPage = listProducts.hasNextPage ?
+    `<li class="page-item"><a class="page-link" href="#" onclick="onFilterChange(${listProducts.nextPage})"> --></a></li>` : '';
+
+  //Render dinamicos de page-item para paginado  
+  const paginationHTML = `
+    ${previousPage}
+    <li class="page-item"><a class="page-link">${listProducts.page} de ${listProducts.totalPages} paginas</a></li>
+    ${nextPage}
+  `;
+
+  pagination.innerHTML = paginationHTML;
+});
+
+
+
+socket.on("dinamic-list-cart", (cartUpdt) => {
+  const cartBody = document.getElementById("dinamic-list-cart");
+  const cartText = cartUpdt.cartProducts.map((cartProduct) => `
+    <p class="card-text">${cartProduct.idProduct.title} x ${cartProduct.quantity}</p>  
+  `);
+
+  cartBody.innerHTML = cartText.join("");
+});
+
+
+function onFilterChange(page) {
+  const filterLimit = parseInt(document.getElementById("filterLimit").value);
+  const filterSort = document.getElementById("filterSort").value;
+  const filterAttName = document.getElementById("filterAttName").value;
+  const filterText = document.getElementById("filterText").value;
+  const filterPage = page ? page : 1
+
+  socket.emit("onFilterChange", filterLimit, filterPage, filterSort, filterAttName, filterText);
+}
+
+
+function addToCart(productId) {  
+  socket.emit("addToCart", productId);
+}
