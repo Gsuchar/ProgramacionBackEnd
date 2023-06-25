861fa41eb7ddebf30fad5f55b8d995bf3a216ce4
Gsuchar <jugasu06@gmail.com>
2023-06-25 02:26:17 -0300

    9_Passp-Gith

--- Entregables/9_Login-Passport-GitHub/src/routes/authRoutes.js
+++ Entregables/9_Login-Passport-GitHub/src/routes/authRoutes.js
@@ -59,7 +59,7 @@ authRouter.get('/perfil', isUser, (req, res) => {
 });
 
 authRouter.get('/administracion', isUser, isAdmin, (req, res) => {
-  return res.send('datos super secretos clasificados sobre los nuevos ingresos a boca juniors');
+  return res.send('Si ves esto es que sos ADMIN.');
 });
 
 
