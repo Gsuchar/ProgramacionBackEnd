1a7a4cb7da12282c17ffed5f44230696a0ca59d8
Gsuchar <jugasu06@gmail.com>
2023-07-31 20:42:27 -0300

    3erEntrega-PF

--- Entregables/12_3erEntrega-PF/src/services/ticketService.js
+++ Entregables/12_3erEntrega-PF/src/services/ticketService.js
@@ -5,18 +5,14 @@ export class TicketService {
     async addTicket(purchaser, ticket, totalCart) {
         try {
           const ticketData = {
-            code: "", // Función para generar el código único
-            purchase_datetime: new Date(), // Fecha y hora actual
+            code: "", 
+            purchase_datetime: new Date(), 
             amount: totalCart,
             purchaser: purchaser,
             products : ticket            
         };              
         const savedTicket = await ticketModel_2.addTicket(ticketData);
         console.log("serv TICKET>>>  "+ JSON.stringify(savedTicket))
-        //savedTicket.map((t) => savedTicket.code =="" ?   { ...t, code: t._id } : '');
-        // if (savedTicket.code === "") {
-        //     savedTicket.code = savedTicket._id;
-        // }
         savedTicket.code === "" ? savedTicket.code = savedTicket._id: '' ;
         return savedTicket;
         } catch (error) {
@@ -52,15 +48,6 @@ export class TicketService {
             throw new Error("ERROR.");
         }
     }
-    
-
-    // // Función para generar un código único para el ticket
-    // function generateUniqueCode() {
-    //     // Aquí implementa la lógica para generar un código único para el ticket
-    //     // Puedes utilizar alguna librería como `uuid` para esto.
-    //     // Ejemplo con uuidv4: return uuidv4();
-    // }        
-    
 
  //FIN LLAVE TICKETSERVICE    
 };
