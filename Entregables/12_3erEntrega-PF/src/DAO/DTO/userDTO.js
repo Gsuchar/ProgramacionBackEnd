d9fbaa6633cb61c33ce4519c9a37b871e4a74c82
Gsuchar <jugasu06@gmail.com>
2023-08-01 19:43:04 -0300

    3erEntrega-PF

---
+++ Entregables/12_3erEntrega-PF/src/DAO/DTO/userDTO.js
@@ -0,0 +1,11 @@
+export class UserDTO{
+    constructor(user){
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.role = user.role;
+    }
+
+}
+  
+  
--- Entregables/12_3erEntrega-PF/src/controllers/sessionController.js
+++ Entregables/12_3erEntrega-PF/src/controllers/sessionController.js
@@ -1,8 +1,12 @@
 //@ts-check
+import { UserDTO } from "../DAO/DTO/userDTO.js";
 class SessionsController {
     currentSession(req, res) {
-      return res.send(JSON.stringify(req.session));
+      const user = new UserDTO(req.session.user)
+      return res.send(JSON.stringify(user));
+
     }
+
      dashboard(req, res) {
       req.session.user = req.user;
       // Logueado va al dashboard.
