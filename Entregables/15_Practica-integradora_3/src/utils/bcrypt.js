529a2bf4a41ef2b96b97908f20975507654fa80f
Gsuchar <jugasu06@gmail.com>
2023-10-10 19:18:18 -0300

    .

--- Entregables/15_Practica-integradora_3/src/utils/bcrypt.js
+++
@@ -1,7 +0,0 @@
-//----------------bcrypt------------------------------
-import bcrypt from 'bcrypt';
-export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-export const isValidPassword = (password, hashPassword) => bcrypt.compareSync(password, hashPassword);
-
-
-/***************************************************************************/
